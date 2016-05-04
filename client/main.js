857323e79eb71b535387ec7be44a19740e548f3f
mPemba <michael.j.say@gmail.com>
2016-05-03 22:10:18 -0600

    playing with D3js and guess what, it is amazing.

--- client/main.js
+++ client/main.js
@@ -1,55 +1,55 @@
-// import { Template } from 'meteor/templating';
-// import { ReactiveVar } from 'meteor/reactive-var';
-//
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
 import './main.html';
-//
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-//
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-//
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
 
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
 
-var parseDate = d3.time.format("%d-%b-%y").parse;
+  var margin = {top: 30, right: 20, bottom: 30, left: 50},
+      width = 600 - margin.left - margin.right,
+      height = 270 - margin.top - margin.bottom;
 
-var x = d3.time.scale().range([0, width]);
-var y = d3.scale.linear().range([height, 0]);
+  // this line parses the time and makes it flexible as fuck.
+  var parseDate = d3.time.format("%d-%b-%y").parse;
 
-var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(5);
+  var x = d3.time.scale().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
 
-var yAxis = d3.svg.axis().scale(y)
-    .orient("left").ticks(5);
+  var xAxis = d3.svg.axis().scale(x)
+      .orient("bottom").ticks(5);
 
-var valueline = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+  var yAxis = d3.svg.axis().scale(y)
+      .orient("left").ticks(5);
 
-var svg = d3.select("body")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+  var valueline = d3.svg.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.close); });
 
-// Get the data
-d3.csv("data.csv", function(error, data) {
+  var svg = d3.select("body")
+      .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+  var dataDates = [
+    {date:"1-May-12",close:"48.13"},
+    {date:"30-Apr-12",close:"53.98"},
+    {date:"27-Apr-12",close:"67.00"},
+    {date:"26-Apr-12",close:"89.70"},
+    {date:"25-Apr-12",close:"99.00"}
+
+    // example times
+    // {date:"23-May-12",close:"666"},
+    // {date:"26-May-14",close:"69"}
+  ];
+
+  var blastMeData = function(data) {
+
+
     data.forEach(function(d) {
         d.date = parseDate(d.date);
         d.close = +d.close;
@@ -72,4 +72,22 @@ d3.csv("data.csv", function(error, data) {
         .attr("class", "y axis")
         .call(yAxis);
 
+  }
+
+  blastMeData(dataDates);
+
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+
+  },
 });
